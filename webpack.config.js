2c33339622791fa253bead79ec7da2a59817953f
Jed <johon8@gmail.com>
2017-06-15 21:53:38 -0400

    project file updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,72 @@
 const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const packageData = require("./package.json");
-module.exports = {
-    entry: './src/library.js',
-    output: {
-        path: path.resolve(__dirname, "dist"),
+const env  = require('yargs').argv.env;
+
+let entryPoint = './src/library.js';
+let plugins = [];
+let output = null;
+let external = {
+    jquery: {
+        commonjs: 'jquery',
+        commonjs2: 'jquery',
+        amd: 'jquery',
+        root: '$'
+    },
+    'pd-sputil': {
+        commonjs: 'pd-sputil',
+        commonjs2: 'pd-sputil',
+        amd: 'pd-sputil',
+        root: 'pdsputil'
+    }
+};
+
+if (env === 'dev' || env === 'build') {
+    entryPoint = './src/library.js';
+    output = {
+        path: path.resolve(__dirname, "./dist"),
         filename: `${packageData.name}.js`,
         libraryTarget: 'umd',
         library: 'pdspserverjsom' //this will be the global variable to hook into
-    },
+    };
+}
+if(env === 'build') {
+    output.filename = `${packageData.name}.min.js`;
+    plugins.push(new UglifyJsPlugin({ minimize: true }));
+}
+if(env === 'test') {
+    entryPoint = './project_tests.js';
+    output = {
+        path: path.resolve(__dirname, "./tests"),
+        filename: "spServerJsom_tests.js",
+    };
+    external['./src/library.js'] = "pdspserverajax";
+    external.jquery = "jQuery";
+    external['pd-sputil'] = "pdsputil";
+}
+
+module.exports = {
+    entry: entryPoint,
+    output: output,
     module:{
         rules:[
             {  
                 test: /\.js$/,
-                exclude: /node_modules/,
+                //exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['es2015']
+                        presets: [
+                             ['es2015', {modules: false}]
+                        ]
                     }
                 }
             }
         ]
     },
-    externals: {
-        "jquery": {
-            commonjs: 'jquery',
-            commonjs2: 'jquery',
-            amd: 'jquery',
-            root: '$'
-        }
-    },
-    devtool: 'source-map'
+    plugins: plugins,
+    externals: external,
+    //devtool: 'source-map'
 };
 
